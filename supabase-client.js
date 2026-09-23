b50443376d36e0e55ce759685bf18aeed90b407d
LopomoEdu <96213532+EduUDlop@users.noreply.github.com>
2026-09-23 20:41:37 -0300

    Delete supabase-client.js
    sim

--- supabase-client.js
+++
@@ -1,5 +0,0 @@
-// supabase-client.js
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.47.0/+esm';
-import { SUPABASE_URL, SUPABASE_ANON_KEY } from './config.js';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
